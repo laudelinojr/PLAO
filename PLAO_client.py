99a2870e4517cbb03a20846b82c86f886fb60116
laudelinojr <laudelinojr@gmali.com>
2021-03-22 01:50:19 -0300

    alteracoes

--- PLAO_client.py
+++ PLAO_client.py
@@ -133,7 +133,10 @@ def OpenStackHypervisorStats(): #criar file hypervisor_stats.txt in openstack di
 def GetJitter(CLOUDTOIP,QUANTITY_PCK,STATUS):
     #if STATUS == "CLIENT":
     #iperf = subprocess.run(["iperf3", "-s", "-1", "-D"])
-    iperf2 = subprocess.check_output(["iperf3", "-c", CLOUDTOIP,"-u", "-t", QUANTITY_PCK])
+    try:
+        iperf2 = subprocess.check_output(["iperf3", "-c", CLOUDTOIP,"-u", "-t", QUANTITY_PCK])
+    except:
+        return ''
     jitter = iperf2.split()[-11]
     print (jitter)
     resp = str(jitter, 'utf-8')
