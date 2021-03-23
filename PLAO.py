2f55556e2435c705bfd40cb2d60f87f9d7579acc
laudelinojr <laudelinojr@gmali.com>
2021-03-23 11:54:03 -0300

    altercoes

--- PLAO.py
+++ PLAO.py
@@ -225,7 +225,7 @@ def conectado(connection, enderecoCliente):
                     commands.update({(ID): {'CLOUD': CLOUD,'CLOUDIP': CLOUDIP, 'DATEHOUR': DATEHOUR,'CLOUDTONAME': CLOUDTONAME, 'CLOUDTOIP': CLOUDTOIP, 'STATUS': STATUS, 'PRICE': PRICE, 'LATTENCY': LATENCY, 'JITTER': JITTER , 'CPU': CPU , 'MEMORY': MEMORY ,'CONEXAO': connection}})
                 if TIPO == 'SENDS':  #check the type protocol
 
-                    CLOUD_STATUS_CPU=int(clouds.get(str(CLOUD_COD)).get('CPU'))
+                    CLOUD_STATUS_CPU=int(clouds.get(str(ID).get('CPU'))
                     if int(CPU) > THRESHOLD and CLOUD_STATUS_CPU == 0:
                         CPU_STATUS_NOW=1   #Values: 0-cpu normal, 1-cpu high and cost value going to change
                         VIMURL=clouds.get(str(ID)).get('VIMURL')
