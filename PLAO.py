b4eafe2d956b41cfaa56c757009130c6e5bf8603
laudelinojr <laudelinojr@gmali.com>
2021-03-23 11:35:12 -0300

    altercoes

--- PLAO.py
+++ PLAO.py
@@ -28,7 +28,7 @@ import time
 debug=0
 
 #requisites for change all price of specif VIM (Cloud)
-GROW_ADD_PRICE=10 #Number to add vnf Price
+DOWN_UP_PRICE=10 #Number to add vnf Price
 THRESHOLD=90 #THRESHOLD of CPU to apply rate in Cloud's price
 
 #requisites1 SearchChangeVNFDPrice
@@ -87,7 +87,7 @@ def SearchChangeVNFDPrice(NAME_VNFD,VIM_URL,PRICE_VNFD):
     else:
         if debug ==1: print ("DEBUG: File not changed")
 
-def SearchGrowUpVimPrice(VIM_URL,GROW_ADD_PRICE,CLOUD_COD,STATUS_CPU_NOW,DATEHOUR):
+def SearchDownUpVimPrice(VIM_URL,UP_DOWN_PRICE,CLOUD_COD,STATUS_CPU_NOW,DATEHOUR):
     #Receive the CPU STATUS NOW and update in list cloud, the CLOUD_STATUS_CPU
     A=open(FILE_VNF_PRICE, )
     B=yaml.full_load(A)
@@ -107,11 +107,15 @@ def SearchGrowUpVimPrice(VIM_URL,GROW_ADD_PRICE,CLOUD_COD,STATUS_CPU_NOW,DATEHOU
                     if PRICE > 0:
                         if STATUS_CPU_NOW == 1 and CLOUD_STATUS_CPU == 0: #If the cloud CPU now is high (1), but in dict is normal (0), we need change dict to(1);
                             clouds.update({str(CLOUD_COD):{'CLOUD': CLOUD,'CLOUDIP':CLOUDIP,'VIMURL': VIMURL,'CPU':'1'}})
-                            B[i]['prices'][f]['price']=PRICE+GROW_ADD_PRICE #Change the VNF Price with the rate price
+                            B[i]['prices'][f]['price']=PRICE+UP_DOWN_PRICE #Change the VNF Price with the rate price
                             CHANGED_PRICE_VIM_URL=CHANGED_PRICE_VIM_URL+1 #count auxiliar variable
-                        if STATUS_CPU_NOW == 0 and CLOUD_STATUS_CPU == 1: #If the cloud CPU now is ok (0), but in dict is high (1), we need change dict to (0)
+                        if STATUS_CPU_NOW == 0 and CLOUD_STATUS_CPU == 1 and PRICE >= UP_DOWN_PRICE: #If the cloud CPU now is ok (0), but in dict is high (1), we need change dict to (0)
                             clouds.update({str(CLOUD_COD):{'CLOUD': CLOUD,'CLOUDIP':CLOUDIP,'VIMURL': VIMURL,'CPU':'0'}})
-                            B[i]['prices'][f]['price']=int(PRICE+GROW_ADD_PRICE) #Change the VNF Price with the rate price
+                            B[i]['prices'][f]['price']=int(PRICE-UP_DOWN_PRICE) #Change the VNF Price with the rate price
+                            CHANGED_PRICE_VIM_URL=CHANGED_PRICE_VIM_URL+1 #count auxiliar variable
+                        if STATUS_CPU_NOW == 0 and CLOUD_STATUS_CPU == 1 and PRICE =< UP_DOWN_PRICE: #If the cloud CPU now is ok (0), but in dict is high (1), we need change dict to (0)
+                            clouds.update({str(CLOUD_COD):{'CLOUD': CLOUD,'CLOUDIP':CLOUDIP,'VIMURL': VIMURL,'CPU':'0'}})
+                            B[i]['prices'][f]['price']=0 #Change the VNF Price with the rate price
                             CHANGED_PRICE_VIM_URL=CHANGED_PRICE_VIM_URL+1 #count auxiliar variable
                     else:
                         print("Trying update nvf_price file. There are price<=0. Check this.")
@@ -224,7 +228,9 @@ def conectado(connection, enderecoCliente):
                     if int(CPU) > THRESHOLD:
                         CPU_STATUS_NOW=1   #Values: 0-cpu normal, 1-cpu high and cost value going to change
                         VIMURL=clouds.get(str(ID)).get('VIMURL')
-                        SearchGrowUpVimPrice(VIMURL,GROW_ADD_PRICE,ID,CPU_STATUS_NOW,DATEHOUR) #The cost is add by CPU bigger
+                        SearchDownUpVimPrice(VIMURL,DOWN_UP_PRICE,ID,CPU_STATUS_NOW,DATEHOUR) #The cost is add by CPU bigger
+                    
+                    #SE o thrsshold voltar para baixo? voltar ao normal??? ver codigo para isto
 
                     nomearquivo1=PATH_LOG+CLOUD+'_'+CLOUDIP+'_history.txt' #write data in file
                     nomearquivo2=PATH_LOG+'LINK_'+CLOUD+'_history.txt' #write data in file
