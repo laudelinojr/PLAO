6a948dc7c794ed5a7c176acf033408e4f7b9a95a
laudelinojr <laudelinojr@gmali.com>
2021-03-23 19:02:00 -0300

    alteracoes

--- script.sh
+++ script.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-#if [$1 -eq "vnf_price_list.yaml"]
-#then
-# `/sbin/runuser -l mano -c  'docker cp /opt/PLAO/osm/vnf_price_list.yaml $(docker ps -qf name=osm_pla):/placement/'`
+if [ $1 == "vnf_price_list.yaml" ];
+then
+#echo 1 #`/sbin/runuser -l mano -c  'docker cp /opt/PLAO/osm/vnf_price_list.yaml $(docker ps -qf name=osm_pla):/placement/'`
+ docker cp /opt/PLAO/osm/vnf_price_list.yaml $(docker ps -qf name=osm_pla):/placement/
+ echo date "- Arquivo vnf_price_list.yaml atualizado para o  placement OSM." >> /opt/PLAO/log/copy_to_docker.txt
+fi
 
-#fi
-
-#if [$1 -eq "pill_price_list.yaml"]
-#then
-# `/sbin/runuser -l mano -c  'docker cp /opt/PLAO/osm/pill_price_list.yaml $(docker ps -qf name=osm_pla):/placement/'`
-#fi
-
-docker cp /opt/PLAO/osm/vnf_price_list.yaml $(docker ps -qf name=osm_pla):/placement/
-docker cp /opt/PLAO/osm/pil_price_list.yaml $(docker ps -qf name=osm_pla):/placement/
-
-echo date "- copiado arquivos para container placement" >> /opt/PLAO/log/copy_to_docker.txt
+if [ $1 == "pil_price_list.yaml" ];
+then
+#echo 2 # `/sbin/runuser -l mano -c  'docker cp /opt/PLAO/osm/pill_price_list.yaml $(docker ps -qf name=osm_pla):/placement/'`
+ docker cp /opt/PLAO/osm/pil_price_list.yaml $(docker ps -qf name=osm_pla):/placement/
+ echo date "- Arquivo pil_price_list.yaml atualizado para o  placement OSM." >> /opt/PLAO/log/copy_to_docker.txt
+fi
