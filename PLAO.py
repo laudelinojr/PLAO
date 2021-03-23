ee4eed0cad6e130a3114027247793f28f50b00ac
laudelinojr <laudelinojr@gmali.com>
2021-03-23 12:15:55 -0300

    alteracoes

--- PLAO.py
+++ PLAO.py
@@ -226,12 +226,12 @@ def conectado(connection, enderecoCliente):
                 if TIPO == 'SENDS':  #check the type protocol
 
                     CLOUD_STATUS_CPU=int(clouds.get(str(ID).get('CPU'))
-                    if int(CPU) > THRESHOLD and CLOUD_STATUS_CPU == 0:
+                    if (int(CPU) > THRESHOLD) and (CLOUD_STATUS_CPU == 0):
                         CPU_STATUS_NOW=1   #Values: 0-cpu normal, 1-cpu high and cost value going to change
                         VIMURL=clouds.get(str(ID)).get('VIMURL')
                         SearchDownUpVimPrice(VIMURL,DOWN_UP_PRICE,ID,CPU_STATUS_NOW,DATEHOUR) #The cost is add by CPU bigger
 
-                    if int(CPU) < THRESHOLD and CLOUD_STATUS_CPU == 1:
+                    if (int(CPU) < THRESHOLD) and (CLOUD_STATUS_CPU == 1):
                         CPU_STATUS_NOW=0   #Values: 0-cpu normal, 1-cpu high and cost value going to change
                         VIMURL=clouds.get(str(ID)).get('VIMURL')
                         SearchDownUpVimPrice(VIMURL,DOWN_UP_PRICE,ID,CPU_STATUS_NOW,DATEHOUR) #The cost is add by CPU bigger
