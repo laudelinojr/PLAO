01b2c8aae4ba1675ae13386e09a3463eb12cebf8
laudelinojr <laudelinojr@gmali.com>
2021-03-23 22:54:13 -0300

    alteracoes

--- PLAO_client.py
+++ PLAO_client.py
@@ -221,10 +221,10 @@ try:
                         CPU=GetCpuSO()
                         NVM=GetHypervisorStats(CLOUDIP,"running_vms")                       
                         print ('nvm: '+ NVM)
-                        CPUC=GetHypervisorStats(CLOUDIP,"vcpu_use_percent")
-                        print ('cPUC: '+CPUC + (type(CPUC)))
-                        MEMORY=MemorySO()
-                        print ("MEMORY:"+MEMORY+ (type(MEMORY)))
+                        #CPUC=GetHypervisorStats(CLOUDIP,"vcpu_use_percent")
+                        #print ('cPUC: '+CPUC + (type(CPUC)))
+                        #MEMORY=MemorySO()
+                        #print ("MEMORY:"+MEMORY+ (type(MEMORY)))
                         mensagem = 'SENDS#' + ID + '#' + CLOUD + '#' + CLOUDIP + '#' + DATAHORAC() + '#' + CLOUDTONAME + '#' + CLOUDTOIP + '#' + STATUS + '#' + PRICE + '#' + LATENCY + '#' + JITTER + '#' + CPU + '#' + MEMORY + '#' + DISK + '#' + NVM + '#' + CPUC + '#'
                         print (mensagem)
                         tcp.sendall(mensagem.encode('utf8')) #send to server colletion data
