e1bad24905e4f2e49ab40e636db40dd8baad63e5
laudelinojr <laudelinojr@gmali.com>
2021-03-24 04:04:15 -0300

    alteracoes

--- docker_pla.py
+++ docker_pla.py
@@ -7,7 +7,7 @@ FILE_PIL_PRICE="/opt/PLAO/osm/pil_price_list.yaml"
 #os.system('docker cp '+FILE_VNF_PRICE+' '+'$(docker ps -qf name=osm_pla):/placement/')
 #os.system('docker cp '+FILE_PIL_PRICE+' '+'$(docker ps -qf name=osm_pla):/placement/')
 
-if sys.argv[1] == 'vnf_price_list.yaml':
+if sys.argv[1] == 'vnf_price_list':
     os.system('docker cp '+FILE_VNF_PRICE+' '+'$(docker ps -qf name=osm_pla):/placement/')
 
 if sys.argv[1] == 'pil_price_list':
