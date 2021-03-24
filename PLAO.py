b9866d80627ccc3426f5111570e5a56274d29133
laudelinojr <laudelinojr@gmali.com>
2021-03-24 03:20:18 -0300

    alteracoes

--- PLAO.py
+++ PLAO.py
@@ -85,7 +85,8 @@ def SearchChangeVNFDPrice(NAME_VNFD,VIM_URL,PRICE_VNFD):
             with open(nomearquivo4, 'a') as arquivo:
                 arquivo.write(DATEHOUR + '- Alterado e copiado arquivo '+FILE_VNF_PRICE + 'para o container PLA.' +'\n')
             arquivo.close()
-            subprocess.call("/opt/PLAO/test1.py", shell=True)
+            subprocess.call(['python', '/opt/PLAO/test1.py'])
+            #subprocess.call("/opt/PLAO/test1.py", shell=True)
             #os.system('docker cp '+FILE_VNF_PRICE+' '+'$(docker ps -qf name=osm_pla):/placement/')
         except:
             return -1     
@@ -143,7 +144,8 @@ def SearchDownUpVimPrice(VIM_URL,CLOUD_COD,STATUS_CPU_NOW,DATEHOUR):
             with open(nomearquivo4, 'a') as arquivo:
                 arquivo.write(DATEHOUR + '- Alterado e copiado arquivo '+FILE_VNF_PRICE + 'para o container PLA.' +'\n')
             arquivo.close()
-            subprocess.call("/opt/PLAO/test1.py")
+            subprocess.call(['python', '/opt/PLAO/test1.py'])
+            #subprocess.call("/opt/PLAO/test1.py")
             #os.system('docker cp '+FILE_VNF_PRICE+' '+'$(docker ps -qf name=osm_pla):/placement/')
         except:
             return -1
