22dacdf57c9fbc4223ace579d858e7397883ddb4
laudelinojr <laudelinojr@gmali.com>
2021-03-25 02:57:16 -0300

    alteracoes

---
+++ PLAOsub_client.py
@@ -0,0 +1,97 @@
+import socket
+import time
+import sys
+import datetime
+import platform
+import subprocess
+import psutil
+import os
+import datetime
+import uuid
+
+
+#Debug mode is 1
+debug=1
+
+# The values in config files of OSM need to be integer
+QUANTITY_PCK="5" #Quantity packages to use in ping command 
+DIR_IPERF="C:/Temp/artigo/utils/iperf/" #directory of iperf application
+
+def DATAHORAC():
+    DATAHORAC = datetime.datetime.now().strftime('%d.%m.%y-%H:%M:%S')  # convert hour to string
+    return DATAHORAC
+
+def GetLatency(TARGET,QUANTITY_PCK):
+    #Test with ping to get latency.
+    if platform.system().lower() == "linux":
+        try:
+            ping = subprocess.check_output(["ping", "-c", QUANTITY_PCK, TARGET])
+        except:
+            return "" #if return with error, return empty
+        latency = ping.split()[-2]
+        resp = str(latency, 'utf-8')
+        resp2= resp.split("/")[2]
+        return resp2
+    else: # platform.system().lower() == "windows":
+        try:
+            ping = subprocess.check_output(["ping", "-n", QUANTITY_PCK, TARGET])
+        except:
+            return "" #if return with error, return empty
+        latency = ping.split()[-1]
+        resp = str(latency, 'utf-8')
+        resp2= resp.split("=")[0]
+        resp3=resp2.split("ms")[0]
+        return resp3
+
+
+
+ID_CONF="" #Local variable with uniq identifier cloud
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest = (HOST,PORT)
+tcp.connect(dest)
+
+print ('Starting PING Collector ... ')
+print ('To quit, use CTRL+C\n')
+
+#First comunication with the server
+if HOST == '127.0.0.1':
+    CLOUDTOIP=CLOUDIP_LOCAL
+    mensagem = 'CVNFSENDS#' + '1' + '#' + CLOUDNAME_LOCAL + '#' + 'CLOUDIP_LOCAL' + '#' + 'DATAHORAC()' + '#' + 'CLOUDTONAME' + '#' + CLOUDTOIP + '#' + 'STATUS' + '#' + 'PRICE' + '#' + 'LATENCY' + '#' + '0' + '#' + '0' + '#' + 'MEMORY'+ '#' + 'DISK'+ '#' + 'NVM' + '#' + '0' + '#' + 'MEMORYC' + '#' + 'DISKC' + '#'
+    tcp.sendall(mensagem.encode('utf8')) #Sending to Server    
+else:
+    mensagem = 'REGIS' + '#' + 'ID' + '#' +CLOUDNAME_LOCAL + '#' + CLOUDIP_LOCAL + '#' + 'DATAHORAC()' + '#' + 'CLOUDTONAME' + '#' + 'CLOUDTOIP' + '#' + 'STATUS' + '#' + 'PRICE' + '#' + 'LATENCY' + '#' + '0' + '#' + '0' + '#' + 'MEMORY'+ '#' + 'DISK'+ '#' + 'NVM' + '#' + '0' + '#' + 'MEMORYC' + '#' + 'DISKC' + '#'
+    tcp.sendall(mensagem.encode('utf8')) #Sending to Server
+
+try:
+    while True:
+        msg = tcp.recv(1024).decode("utf8")  #receive the message socket in byte and to convert in utf-8
+        msg = msg.split('#')  #split message separeted in # symbol
+        if len(msg) > 5:
+            TIPO = msg[0] 
+            ID = msg[1]
+            CLOUD = msg[2] #NAME CLOUD
+            CLOUDIP = msg[3] #IP CLOUD
+            DATEHOUR = msg[4] #DATA AND HOUR
+            USERIP = msg[5] #
+            VNFD = msg[6] #IP OF CLOUD TO
+            COMANDO = msg[7] 
+            LATENCY = msg[9] #LATENCY
+
+            if TIPO == 'PINGSENDC':
+                if CLOUDIP_LOCAL == CLOUDIP:
+                    time.sleep(2)
+                    if (CLOUDTOIP != "CLOUDTOIP" ):
+                        LATENCY=str(round(float(GetLatency(CLOUDTOIP,QUANTITY_PCK)))) #Get latency with ping, is necessary set quantity packages
+                        PRICE=LATENCY
+                    print(mensagem)
+                    tcp.sendall(mensagem.encode('utf8')) #send to server colletion data
+        if not msg: break
+except KeyboardInterrupt:
+    print('Tecla de interrupção acionada, saindo... e solicitando exclusao do dispositivo no servidor.')
+except Exception as e:
+    print("Erro no cliente. " + str(e))
+finally:
+    mensagem = 'EXCL#' + ID + '#' + CLOUD + '#' + CLOUDIP + '#' + 'DATAHORAC()' + '#' + 'CLOUDTONAME' + '#' + 'CLOUDTOIP' + '#' + 'STATUS' + '#' + 'PRICE' + '#' +'LATENCY' + '#' + 'JITTER' + '#' + 'CPU' + '#' + 'MEMORY'+ '#' + 'DISK'+ '#' + 'NVM' + '#' + 'CPUC' + '#'
+    tcp.sendall(mensagem.encode('utf8'))
+    tcp.close()
---
+++ PLAOsub_server.py
@@ -0,0 +1,112 @@
+import yaml
+import socket
+import threading
+import time
+import subprocess
+
+#Debug mode is 1
+debug=0
+
+#requisites1 SearchChangeVNFDPrice
+FILE_VNF_PRICE="/opt/PLAO/osm/vnf_price_list.yaml"
+NAME_VNFD="VNFB"
+#VIM_URL='http://10.159.205.6:5000/v3'
+PRICE_VNFD=14
+
+#requisites SearchChangePriceLatencyJitterPIL
+FILE_PIL_PRICE="/opt/PLAO/osm/pil_price_list.yaml"
+PATH_LOG='/opt/PLAO/log/'
+
+def printCloudsDict():
+    while True:
+        time.sleep(15)
+        print('Printing Cloud Dict:')
+        if len(clouds) > 0:
+            for i in clouds:
+                print (i+str(clouds.get(i)))
+        else:
+            print('Cloud Dict Empty')
+
+def DATEHOURS():
+    DATEHOUR = datetime.datetime.now().strftime('%d.%m.%y-%H:%M:%S')  # converte hora para string do cliente
+    return DATEHOURS
+
+def conectado(connection, enderecoCliente):
+        print('Conected with', enderecoCliente)
+        while True:
+            msg = connection.recv(1024).decode('utf8')
+            msg = msg.split('#')  # quebra o texto unico com o separador #
+
+            if len(msg) > 5:  # se true indica que existe protocolo trafegando
+                if debug == 1: print ("DEBUG: dentro primeiro while true conectado2")
+
+                TIPO = msg[0] 
+                ID = msg[1]
+                CLOUD = msg[2] #NAME CLOUD
+                CLOUDIP = msg[3] #IP CLOUD
+                DATEHOUR = msg[4] #DATA AND HOUR
+                USERIP = msg[5] #
+                VNFD = msg[6] #IP OF CLOUD TO
+                COMANDO = msg[7] 
+                LATENCY = msg[9] #LATENCY
+             
+                #fico lendo arquivo TXT USERIP,VNFD,COMANDO
+                #se aparecer algo, os clientes irao verificar, fazer o ping e retornar  o valor, que será posto em outro arquivo txt
+                #PING_FILA
+
+                nomearquivo1='user_vnfd.txt'
+                with open(nomearquivo1, 'w') as arquivo:
+                    linha = arquivo.readline()
+                arquivo.close()
+
+                print(linha)
+
+
+                #arquivo.write(DATEHOUR + ','+ USERIP + ","+ CLOUDIP +","+ CPU + "," + MEMORY + "," + NVM + "," + CPUC + "," + MEMORYC + ","+ DISKC +'\n')
+
+                if TIPO == 'PINGSENDS':
+                    sleep.time(5)
+                    mensagem = "teste pingsends"
+                    #USERIP':USERIP,'VNFD': VNFD, 'COMANDO': STATUS
+                    #commands.update({('ID'): {'CLOUD': CLOUD,'CLOUDIP': CLOUDIP, 'DATEHOUR': DATEHOUR,'CLOUDTONAME': CLOUDTONAME, 'CLOUDTOIP': CLOUDTOIP, 'STATUS': STATUS, 'PRICE': PRICE, 'LATTENCY': LATENCY, 'JITTER': JITTER , 'CPU': CPU , 'MEMORY': MEMORY, 'CPUC': CPUC ,'MEMORYC': MEMORYC ,'DISKC': DISKC , 'CONEXAO': connection}})
+                    #mensagem = 'PINGSENDC#' + '1' + '#' + CLOUD + '#' + CLOUDIP + '#' + DATEHOUR + '#'+ CLOUDTONAME + '#' + CLOUDTOIP + '#' + STATUS + '#' + PRICE + '#' + LATENCY + '#' + JITTER + '#' + CPU + '#' + MEMORY + '#' + DISK+ '#' + NVM + '#' + CPUC + '#' + MEMORYC + '#'+ DISKC + '#'  #preparing message
+                    connection.sendall(mensagem.encode('utf8'))  #sending in first time the command to client
+
+                if TIPO == 'PINGSENDC':
+                 #   commands.update({('ID'): {'CLOUD': CLOUD,'CLOUDIP': CLOUDIP, 'DATEHOUR': DATEHOUR,'CLOUDTONAME': CLOUDTONAME, 'CLOUDTOIP': CLOUDTOIP, 'STATUS': STATUS, 'PRICE': PRICE, 'LATTENCY': LATENCY, 'JITTER': JITTER , 'CPU': CPU , 'MEMORY': MEMORY, 'CPUC': CPUC ,'MEMORYC': MEMORYC ,'DISKC': DISKC , 'CONEXAO': connection}})
+                  #  mensagem = 'SENDC#' + ID + '#' + CLOUD + '#' + CLOUDIP + '#' + DATEHOUR + '#'+ CLOUDTONAME + '#' + CLOUDTOIP + '#' + STATUS + '#' + 'PRICE' + '#' + 'LATENCY' + '#' + '0' + '#' + 'CPU' + '#' + 'MEMORY' + '#' + 'DISK' + '#' + 'NVM' + '#' + 'CPUC' + '#'+ 'MEMORYC' + '#'+ 'DISKC' + '#'
+                    mensagem = "testepingsendc"
+                    connection.sendall(mensagem.encode('utf8'))
+
+                if TIPO == 'EXCL': #Delete registry cloud in Dict
+                    if ID.isdigit():
+                        ping.pop(ID)
+        print('Closing connection with client', enderecoCliente)
+        connection.close()
+
+#Configurations for socket enviroment
+socketServer = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+HOST = "0.0.0.0"#address ip to server
+PORT = 6001 #port to server
+socketServer.bind((HOST,PORT))
+socketServer.listen(10)
+print('Starting Controller PLAO Server for OSM')
+
+#Dictionaries for application use.
+commands = {}
+ping = {}
+
+try:
+    while True:
+        con, enderecoCliente = socketServer.accept()
+        IPORIGEM, PORTAORIGEM= enderecoCliente
+        thread_cliente = threading.Thread(target=conectado,args=(con,enderecoCliente,))
+        thread_cliente.start()
+        if debug == 1: print("DEBUG: iniciado thread")
+    print("Finalizando conexao do cliente", IP)
+except KeyboardInterrupt:
+    print('Tecla de interrupção acionada, saindo... Digite "Ctrl + C" mais uma vez.\n')
+except Exception as e:
+    print("Erro no cliente. " + str(e))
+finally:
+    socketServer.close()
