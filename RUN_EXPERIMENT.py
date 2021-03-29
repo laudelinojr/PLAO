d8f17d14a91a15b57c4f9975f81eeb2ab867c641
laudelinojr <laudelinojr@gmali.com>
2021-03-29 10:45:50 -0300

    alteracoes

--- RUN_EXPERIMENT.py
+++ RUN_EXPERIMENT.py
@@ -81,18 +81,18 @@ def deleteAllNS(lista):
         print("Removendo NS: "+lista[i])
 
 def ExecuteCommand(exec_command):
-    try:
-        ret = subprocess.run(exec_command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, executable='/bin/bash')
-        if debug_file == 1:
-            print("DEBUG ON")
-            print("CMD - " + exec_command)
-            print("RETURN - \n" + ret.stdout)
-        return ret.returncode
-    except:
-        print('FAIL IN COMMAND EXECUTE')
-        print("CMD - " + exec_command)
-        print("ERROR - " + ret)
-        return ret.returncode
+#    try:
+    ret = subprocess.run(exec_command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, executable='/bin/bash')
+    #    if debug_file == 1:
+    #        print("DEBUG ON")
+    #        print("CMD - " + exec_command)
+    #        print("RETURN - \n" + ret.stdout)
+    #    return ret.returncode
+    #except:
+    #    print('FAIL IN COMMAND EXECUTE')
+    #    print("CMD - " + exec_command)
+    #    print("ERROR - " + ret)
+    return ret.returncode
 
 COMANDO.insert(0,'cd /opt/PLAO ; git pull; rm -rf logs/*; python3 PLAO.py &')
 COMANDO.insert(1,'ssh 10.159.205.6 cd /opt/PLAO; git pull; python3 PLAO_client.py 10.159.205.10 openstack1 10.159.205.6 &')
