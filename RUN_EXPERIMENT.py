5349391e1451b2cd675e15c3d6cf491011b344c0
laudelinojr <laudelinojr@gmali.com>
2021-03-29 11:54:53 -0300

    alteracoes

--- RUN_EXPERIMENT.py
+++ RUN_EXPERIMENT.py
@@ -82,6 +82,7 @@ def deleteAllNS(lista):
 
 def ExecuteCommand(exec_command):
 #    try:
+    print (exec_command)
     ret = subprocess.run(exec_command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, executable='/bin/bash')
     #    if debug_file == 1:
     #        print("DEBUG ON")
@@ -96,14 +97,13 @@ def ExecuteCommand(exec_command):
 
 COMANDO.insert(0,'ps -ef | grep "PLAO.py" | awk '+"'"+'{print $2}'+"'"+'; cd /opt/PLAO ; git pull; rm -rf log/*; python3 PLAO.py > /dev/null 2>&1 &')
 COMANDO.insert(1,'ssh root@10.159.205.6 '+ "'"+'ps -ef | grep "PLAO_client.py" | awk '+"'"+'{print $2}'+"'"+'; cd /opt/PLAO; git pull; python3 PLAO_client.py 10.159.205.10 openstack1 10.159.205.6 > /dev/null 2>&1 &' +"'")
-#COMANDO.insert(2,'ssh root@10.159.205.12 "cd /opt/PLAO; git pull; python3 PLAO_client.py 10.159.205.10 openstack2 10.159.205.12 > /dev/null 2>&1 &"')
-#COMANDO.insert(3,'python3 USER_TEST.py 1a') #Create NS with 2 VNFD using PLA module OSM sem latencia do usuario
+COMANDO.insert(2,'ssh root@10.159.205.12 '+ "'"+'ps -ef | grep "PLAO_client.py" | awk '+"'"+'{print $2}'+"'"+'; cd /opt/PLAO; git pull; python3 PLAO_client.py 10.159.205.10 openstack1 10.159.205.6 > /dev/null 2>&1 &' +"'")
+COMANDO.insert(3,'python3 USER_TEST.py 1a') #Create NS with 2 VNFD using PLA module OSM sem latencia do usuario
 #COMMANDO=''
 #COMANDO8=''
 #COMANDO9=''
 #COMANDO10=''
-#for pid in $(ps -ef | grep "PLAO_client" | awk '{print $2}'); do kill -9 $pid; done
-#Primeiro cenario
+
 for i in range(len(COMANDO)):
     print ("Executando comando: "+str(i))
     ExecuteCommand(COMANDO[i])
