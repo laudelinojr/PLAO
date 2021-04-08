ebe5ab11f5504a715636026192b0b081b3ae08bb
laudelinojr <laudelinojr@gmali.com>
2021-04-08 18:39:16 -0300

    dados coleta

--- PLAO_client.py
+++ PLAO_client.py
@@ -48,13 +48,13 @@ def DATAHORAC():
 def GetHypervisorStats(OPENSTACK_FROM, PARAMETER):
     # Disable SSL Warnings when using self-signed certificates
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    if ( OPENSTACK_FROM == "10.159.205.7" ):
+    #if ( OPENSTACK_FROM == "10.159.205.7" ):
         # API
-        IDENTITY_API = "http://10.159.205.6:5000/v3"
-    if  ( OPENSTACK_FROM == "10.159.205.13" ):
-        # API
-        IDENTITY_API = "http://10.159.205.12:5000/v3"
-        #IDENTITY_API = "http://"+OPENSTACK_FROM+":5000/v3"
+    #    IDENTITY_API = "http://10.159.205.6:5000/v3"
+    #if  ( OPENSTACK_FROM == "10.159.205.13" ):
+    # API
+    #IDENTITY_API = "http://10.159.205.12:5000/v3"
+    IDENTITY_API = "http://"+OPENSTACK_FROM+":5000/v3"
 
     # OpenStack User and Project. From the OpenRC file.
     PROJECT_NAME = "admin"
