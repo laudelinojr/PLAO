106b0c5ca331ad38f5eca7c04b99abc474d33780
laudelinojr <laudelinojr@gmali.com>
2021-07-29 10:32:06 -0300

    atualizacaoes

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from PLAO_client2 import *
-
-payload = {"ip" : "10.159.205.11"}
-
-a = requests.request(
-    method="POST", url='http://192.168.56.1:3333/plao/', json=payload)
-
-print(a.text)
