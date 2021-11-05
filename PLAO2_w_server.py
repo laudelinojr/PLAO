c2a2e9aa9e06cf3487cf146faaaf2c76734474f9
laudelinojr <laudelinojr@gmali.com>
2021-11-05 08:06:51 -0300

    atualizacaoes

--- PLAO2_w_server.py
+++ PLAO2_w_server.py
@@ -4,7 +4,7 @@ from PLAO2_w_routes import app
 from PLAO2 import *
 
 #servers = Servers()
-IPServerLocal="127.0.0.1"
+IPServerLocal="10.159.205.10"
 
 #Alterar para IP do servidor PLAO
 app.run(IPServerLocal, '3332',debug=True)
