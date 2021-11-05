2bd3fe65729e0fbddd36490e0d6fd03787bbba61
laudelinojr <laudelinojr@gmali.com>
2021-11-04 22:41:19 -0300

    atualizacaoes

--- PLAO2_test_to_server.py
+++ PLAO2_test_to_server.py
@@ -6,6 +6,6 @@ payload = {"ip" : "10.0.19.148"}
 
 # Request to cloud. Is necessary in http URL the cloud ip address
 a = requests.request(
-    method="POST", url='http://127.0.0.1:3332/plaoserver/', json=payload)
+    method="POST", url='http://10.159.205.10:3332/plaoserver/', json=payload)
 
 print(a.text)
