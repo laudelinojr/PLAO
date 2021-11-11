c6e2b54610fca80ac96b24ef08da9824b4df31ab
laudelinojr <laudelinojr@gmali.com>
2021-11-10 23:34:51 -0300

    atualizacaoes

--- PLAO2_w_routes.py
+++ PLAO2_w_routes.py
@@ -17,6 +17,7 @@ def latencia_user_plao():
             #from PLAO_client2 import *
             # The payload is the user ip address.
         payload = {"ip" : "10.0.19.148"}
+        print (payload)
         # Request to cloud. Is necessary in http URL the cloud ip address
         a = requests.request(
             method="POST", url='http://10.159.205.11:3333/plao/', json=payload)
