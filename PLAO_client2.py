a0960e3c46cafac14a696335fc1b5ebcee2379ec
laudelinojr <laudelinojr@gmali.com>
2021-11-12 09:24:18 -0300

    atualizacaoes

--- PLAO_client2.py
+++ PLAO_client2.py
@@ -441,6 +441,45 @@ class CreateThread():
         self.ExecJitter = Jitter()
         self.thread_iperf = threading.Thread(target=self.ExecJitter.execJitter,args=(TARGET,QUANTITY_PCK,LOOP,RESOURCE_ID,GNOCCHI))
         self.thread_iperf.start()
+
+
+@appc.route("/plao/", methods=['POST', 'GET', 'DELETE'])
+def latencia_user_plao():
+    if request.method == "POST":
+        print("entrei aqui")
+        request_data = request.get_json()
+        ip = request_data['ip']
+        ip_user = request_data['ipuser']
+        print (ip)
+        print (ip_user)
+        VarPlao="plao"
+        servers = Servers()
+        IPServerLocal=servers.getSearchIPLocalServer()
+        print ("ipserver: "+IPServerLocal)
+        NameServerLocal=servers.getServerName(IPServerLocal)
+        print ("nameserver: "+NameServerLocal)
+        auth_session = OpenStack_Auth(cloud_name=NameServerLocal)
+        sess = auth_session.get_session()
+        gnocchi = Gnocchi(session=sess)
+        resource_id=gnocchi.get_resource_id(VarPlao)
+        print ("idRrecurso: "+resource_id)
+        print("Checking if metric Latency exists...")      
+        Metric_Lat_test=""
+        Name_Metric_Lat="Lat_To_"+str(ip_user)
+        print(Name_Metric_Lat)
+        Metric_Lat_test=gnocchi.get_metric_id(Name_Metric_Lat,resource_id)
+        if (Metric_Lat_test == ""):
+            print("depois if metric lat test")
+            print("The "+ Name_Metric_Lat + " do not exist. Creating metric Latency.")
+            if (gnocchi.set_create_metric(Name_Metric_Lat,VarPlao,resource_id,"ms") == "MetricaJaExiste" ):
+                print ("Metric already exists.")            
+            else:
+                print("Created Metrics.")
+        print("Iniciar Thread para aguardar pedidos de latencia para usuarios")
+        Thread_Lat = CreateThread()
+        print(Thread_Lat.ThreadPing(ip_user,"5","0",resource_id,gnocchi))
+        return "ok"
+
         
 
 def main():
@@ -464,7 +503,7 @@ def main():
 
 
 
-    
+
     print("Starting PLAO client...")
     print("Reading servers.yaml ...")
 
@@ -559,47 +598,6 @@ def main():
         #to create thread for Latency
         Thread_Jitt = CreateThread()
         Thread_Jitt.ThreadIperf(IpOthersServers.get(i).get('ip'),"5","1",resource_id,gnocchi)
-
-
-
-@appc.route("/plao/", methods=['POST', 'GET', 'DELETE'])
-def latencia_user_plao():
-    if request.method == "POST":
-        print("entrei aqui")
-        request_data = request.get_json()
-        ip = request_data['ip']
-        ip_user = request_data['ipuser']
-        print (ip)
-        print (ip_user)
-        VarPlao="plao"
-        servers = Servers()
-        IPServerLocal=servers.getSearchIPLocalServer()
-        print ("ipserver: "+IPServerLocal)
-        NameServerLocal=servers.getServerName(IPServerLocal)
-        print ("nameserver: "+NameServerLocal)
-        auth_session = OpenStack_Auth(cloud_name=NameServerLocal)
-        sess = auth_session.get_session()
-        gnocchi = Gnocchi(session=sess)
-        resource_id=gnocchi.get_resource_id(VarPlao)
-        print ("idRrecurso: "+resource_id)
-        print("Checking if metric Latency exists...")      
-        Metric_Lat_test=""
-        Name_Metric_Lat="Lat_To_"+str(ip_user)
-        print(Name_Metric_Lat)
-        Metric_Lat_test=gnocchi.get_metric_id(Name_Metric_Lat,resource_id)
-        if (Metric_Lat_test == ""):
-            print("depois if metric lat test")
-            print("The "+ Name_Metric_Lat + " do not exist. Creating metric Latency.")
-            if (gnocchi.set_create_metric(Name_Metric_Lat,VarPlao,resource_id,"ms") == "MetricaJaExiste" ):
-                print ("Metric already exists.")            
-            else:
-                print("Created Metrics.")
-        print("Iniciar Thread para aguardar pedidos de latencia para usuarios")
-        Thread_Lat = CreateThread()
-        print(Thread_Lat.ThreadPing(ip_user,"5","0",resource_id,gnocchi))
-        return "ok"
-
-
 
 
 if __name__ == "__main__":
