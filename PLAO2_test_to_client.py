9d2df8cb2d146a27fca790e6bd053c3f87fc32b4
laudelinojr <laudelinojr@gmali.com>
2021-11-14 19:39:57 -0300

    atualizacaoes

--- PLAO2_test_to_client.py
+++ PLAO2_test_to_client.py
@@ -2,11 +2,12 @@ import requests
 from PLAO_client2 import *
 
 # The payload is the user ip address.
-payload = {"ip" : "10.0.19.148"}
+payload = {"ipuser" : "10.0.19.148"}
 
 # Request to cloud. Is necessary in http URL the cloud ip address
 a = requests.request(
     method="POST", url='http://10.159.205.8:3333/start/', json=payload)
+
 
 # Request to cloud. Is necessary in http URL the cloud ip address
 a = requests.request(
