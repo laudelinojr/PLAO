17f44165a16fabb15d47c498d2be8a4feadc8082
laudelinojr <laudelinojr@gmali.com>
2021-11-14 20:46:39 -0300

    atualizacaoes

---
+++ utils/PLAO_client2_w_server.py
@@ -0,0 +1,11 @@
+#File in Clouds
+
+from PLAO_client2_w_routes import app
+from PLAO_client2 import *
+#from PLAO2 import *
+
+servers = Servers()
+IPServerLocal=servers.getSearchIPLocalServer()
+
+#Alterar para IP do servidor do PLAO
+app.run(IPServerLocal, '3333',debug=True)
