662ace382c453a720be64d522764e9f08c852ab6
laudelinojr <laudelinojr@gmali.com>
2021-11-14 20:54:59 -0300

    atualizacaoes

--- PLAO2_w_routes.py
+++ PLAO2_w_routes.py
@@ -10,15 +10,15 @@ app = Flask(__name__)
 def latencia_user_plao():
     if request.method == "POST":
         request_data = request.get_json()
-        ip_user = request_data['ip']
+        ipuser = request_data['ipuser']
         controle=0
-        print (ip_user)
+        print (ipuser)
         print("Teste na nuvem 1")
             #from PLAO_client2 import *
             # The payload is the user ip address.
         nuvem1="10.159.205.8"
         nuvem2="10.159.205.8"
-        payload = {"ip" : "10.0.19.148"}
+        payload = {"ipuser" : "10.0.19.148"}
         print (payload)
         # Request to cloud. Is necessary in http URL the cloud ip address
         a = requests.request(
