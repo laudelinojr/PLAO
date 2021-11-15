ef67453854ffe1401d0de69b236314fb44c092a9
laudelinojr <laudelinojr@gmali.com>
2021-11-14 23:39:15 -0300

    atualizacaoes

--- PLAO2.py
+++ PLAO2.py
@@ -393,52 +393,55 @@ def main():
 #    thread_MonitorDisaggregated2.start()
 
 
+    try:
+        print("teste")
 
 
-    #File in Clouds
-    app = Flask(__name__)
+        #File in Clouds
+        app = Flask(__name__)
 
-    # The payload is the user ip address.
-    nuvem1="10.159.205.8"
-    nuvem2="10.159.205.11"
+        # The payload is the user ip address.
+        nuvem1="10.159.205.8"
+        nuvem2="10.159.205.11"
 
-    @app.route('/start/',methods=['POST'])
-    def start():
-        if request.method == "POST":
-            request_data = request.get_json()
-            payload = request_data['ipuser']
-            a = requests.request(
-                method="POST", url='http://'+nuvem1+':3333/start/', json=payload)
-            print(a.text)
-            a = requests.request(
-                method="POST", url='http://'+nuvem2+':3333/start/', json=payload)
-            print(a.text)
-            return "okstart"
-    @app.route("/plaoserver/", methods=['POST', 'GET', 'DELETE'])
-    def latencia_user_plao():
-        if request.method == "POST":
-            request_data = request.get_json()
-            controle=0
-            print("Inicio Teste na nuvem 1")
-            # Request to cloud. Is necessary in http URL the cloud ip address
-            a = requests.request(
-                method="POST", url='http://'+nuvem1+':3333/plao/', json=request_data)
-            print(a.text)
-            print("Fim Teste na nuvem 1")
-            print("Inicio Teste na nuvem 2")
-            a = requests.request(
-                method="POST", url='http://'+nuvem2+':3333/plao/', json=request_data)
-            print(a.text)
-            print("Fim Teste na nuvem 2")
-            return "okplaoserver"
+        @app.route('/start/',methods=['POST'])
+        def start():
+            if request.method == "POST":
+                request_data = request.get_json()
+                payload = request_data['ipuser']
+                a = requests.request(
+                    method="POST", url='http://'+nuvem1+':3333/start/', json=payload)
+                print(a.text)
+                a = requests.request(
+                    method="POST", url='http://'+nuvem2+':3333/start/', json=payload)
+                print(a.text)
+                return "okstart"
+        @app.route("/plaoserver/", methods=['POST', 'GET', 'DELETE'])
+        def latencia_user_plao():
+            if request.method == "POST":
+                request_data = request.get_json()
+                controle=0
+                print("Inicio Teste na nuvem 1")
+                # Request to cloud. Is necessary in http URL the cloud ip address
+                a = requests.request(
+                    method="POST", url='http://'+nuvem1+':3333/plao/', json=request_data)
+                print(a.text)
+                print("Fim Teste na nuvem 1")
+                print("Inicio Teste na nuvem 2")
+                a = requests.request(
+                    method="POST", url='http://'+nuvem2+':3333/plao/', json=request_data)
+                print(a.text)
+                print("Fim Teste na nuvem 2")
+                return "okplaoserver"
 
 
-    #servers = Servers()
-    IPServerLocal="10.159.205.10"
-    #Alterar para IP do servidor do PLAO
-    app.run(IPServerLocal, '3332',debug=True)
+        #servers = Servers()
+        IPServerLocal="10.159.205.10"
+        #Alterar para IP do servidor do PLAO
+        app.run(IPServerLocal, '3332',debug=True)
 
-    print("teste")
+    except requests.exceptions.ConnectionError as e:
+        print ("Problema conexao com "+e)
 
     #Thread para enviar request 
     ###thread_MonitorLatencyUser = threading.Thread(target=Request_LatencyUser_Cloud1,args=(cloud1_gnocchi,cloud1_resource_id,cloud2,VNFFile,"openstack1","openstack2"))
