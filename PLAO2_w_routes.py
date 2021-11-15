6fd67038cd6d7337cb2f45beb76497b2ca52ad56
laudelinojr <laudelinojr@gmali.com>
2021-11-14 22:42:51 -0300

    atualizacaoes

--- PLAO2_w_routes.py
+++ PLAO2_w_routes.py
@@ -6,35 +6,38 @@ import requests
 
 app = Flask(__name__)
 
+# The payload is the user ip address.
+nuvem1="10.159.205.8"
+nuvem2="10.159.205.11"
+
 @app.route('/start/',methods=['POST'])
 def start():
     if request.method == "POST":
-        payload = {"ipuser" : "10.0.19.148"}
+        request_data = request.get_json()
+        payload = request_data['ipuser']
         a = requests.request(
-            method="POST", url='http://10.159.205.8:3333/start/', json=payload)
+            method="POST", url='http://'+nuvem1+':3333/start/', json=payload)
+        print(a.text)
+        a = requests.request(
+            method="POST", url='http://'+nuvem2+':3333/start/', json=payload)
         print(a.text)
         return "okstart"
 @app.route("/plaoserver/", methods=['POST', 'GET', 'DELETE'])
 def latencia_user_plao():
     if request.method == "POST":
         request_data = request.get_json()
-        ipuser = request_data['ipuser']
+        payload = request_data['ipuser']
         controle=0
-        print (ipuser)
-        print("Teste na nuvem 1")
-            #from PLAO_client2 import *
-            # The payload is the user ip address.
-        nuvem1="10.159.205.8"
-        nuvem2="10.159.205.8"
-        payload = {"ipuser" : "10.0.19.148"}
-        print (payload)
+        #payload = {"ipuser" : "10.0.19.148"}
+        print("Inicio Teste na nuvem 1")
         # Request to cloud. Is necessary in http URL the cloud ip address
         a = requests.request(
-            method="POST", url='http://10.159.205.8:3333/plao/', json=payload)
+            method="POST", url='http://'+nuvem1+':3333/plao/', json=payload)
         print(a.text)
         print("Fim Teste na nuvem 1")
-
-        print("Teste na nuvem 2")
-
+        print("Inicio Teste na nuvem 2")
+        a = requests.request(
+            method="POST", url='http://'+nuvem2+':3333/plao/', json=payload)
+        print(a.text)
         print("Fim Teste na nuvem 2")
-        return "ok"
+        return "okplaoserver"
