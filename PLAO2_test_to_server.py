7735e410827415de5b2d65caa47da4961cb28fb6
laudelinojr <laudelinojr@gmali.com>
2021-11-14 21:30:11 -0300

    atualizacaoes

--- PLAO2_test_to_server.py
+++ PLAO2_test_to_server.py
@@ -6,6 +6,10 @@ payload = {"ipuser" : "10.0.19.148"}
 
 # Request to cloud. Is necessary in http URL the cloud ip address
 a = requests.request(
+    method="POST", url='http://10.159.205.10:3332/start/', json=payload)
+
+# Request to cloud. Is necessary in http URL the cloud ip address
+a = requests.request(
     method="POST", url='http://10.159.205.10:3332/plaoserver/', json=payload)
 
 print(a.text)
