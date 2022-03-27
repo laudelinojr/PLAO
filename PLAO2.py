4af100499800d69adcd31cca971ca44012036a9e
Laudelino <laudelinojr@gmail.com>
2022-03-27 01:08:44 -0300

    testes

--- PLAO2.py
+++ PLAO2.py
@@ -426,6 +426,7 @@ def main():
             a = requests.request(
                 method="POST", url='http://'+nuvem1+':3333/start/', json=payload)
             print(a.text+"Enviando start para "+nuvem1)
+            return "Executado"
             #a = requests.request(
             #    method="POST", url='http://'+nuvem2+':3333/start/', json=payload)
             #print(a.text+"Enviando start para "+nuvem2)
@@ -444,8 +445,10 @@ def main():
                     method="POST", url='http://'+nuvem1+':3333/userlatency/', json=request_data)
                 print(a.text)
                 print("Fim Teste na nuvem 1")
+                return "Executado"
             except Exception:
                 print("erro ao conectar na porta 3333")
+                return "NaoExecutado"
 
                 #print("Inicio Teste na nuvem 2")
                 #a = requests.request(
