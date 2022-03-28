b6cbd4b21cd6d6edd6b47c59f60b46f2b07f4e27
Laudelino <laudelinojr@gmail.com>
2022-03-28 00:49:48 -0300

    testes

--- PLAO_client2.py
+++ PLAO_client2.py
@@ -137,13 +137,13 @@ def startApp():
     for i in IpOthersServers:
         #to create thread for Latency
         Thread_Lat = CreateThread()
-        Thread_Lat.ThreadPing(IpOthersServers.get(i).get('external_ip'),"5","1",resource_id,gnocchi)
+        Thread_Lat.ThreadPing(str(IpOthersServers.get(i).get('external_ip')),"5","1",resource_id,gnocchi)
 
     print("Creating Jiitter Threads to all servers...")
     for i in IpOthersServers:
         #to create thread for Latency
         Thread_Jitt = CreateThread()
-        Thread_Jitt.ThreadIperf(IpOthersServers.get(i).get('external_ip'),"5","1",resource_id,gnocchi)
+        Thread_Jitt.ThreadIperf(str(IpOthersServers.get(i).get('external_ip')),"5","1",resource_id,gnocchi)
     #Mark variable with number 1 for started status on
     global STARTED
     STARTED=1
