d83ff81f13ce7aff9ea38813b26b1321be828743
Laudelino <laudelinojr@gmail.com>
2022-06-18 00:57:15 -0300

    ajustes

--- osm/osm.py
+++ osm/osm.py
@@ -1,6 +1,7 @@
 import requests
 from urls import *
 import time
+import json
 
 def osm_create_token(project_id='admin'):
 
@@ -38,6 +39,22 @@ def osm_get_vim_accounts(token):
     response = requests.request("GET", url, headers=headers, data=payload,verify=False)
     return response.json()
 
+def osm_get_ns(token):
+    # GET /admin/v1/vims Query information about multiple VIMs
+    method_osm = "/nsd/v1/ns_descriptors/"
+    url = url_osm+method_osm
+    url=str(url)
+    payload = {}
+    # token = token.replace('\r','')
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        "Authorization": 'Bearer '+str(token)
+    }
+    response = requests.request("GET", url, headers=headers, data=payload,verify=False)
+    return response.json()
+
 def geturls(url):
         
     urls = { 'url_projects'  : '/osm/admin/v1/projects',  
@@ -49,7 +66,8 @@ def geturls(url):
     'url_ns_descriptor' : '/osm/nsd/v1/ns_descriptors_content',
     'url_vim_accounts' : '/osm/admin/v1/vim_accounts',
     'url_ns_instance' : '/osm/nslcm/v1/ns_instances',
-    'url_osm' : '/osm'
+    'url_osm' : '/osm',
+    'ns_descriptors' : '/nsd/v1/ns_descriptors'
     }
     return "https://"+IP+":"+PORT+urls.get(url)
 
@@ -64,24 +82,42 @@ def check_token_valid(token):
         osm_create_token()
 
 token = osm_create_token()
-check_token_valid(token)
-
+#check_token_valid(token)
 print(token)
 token2 = token['id']
-token3 = token['expires']
-check_token_valid(token)
-VIM_ACCOUNTS=osm_get_vim_accounts(token)
-print(type(VIM_ACCOUNTS))
+#token3 = token['expires']
+#check_token_valid(token)
 
-print (token3)
+
+
+retorno=osm_get_ns(token2)
+
+#retorno.get('vld')
+
+for i in (retorno):
+    VNFDLIST=()
+    VLD=(i['vld'])
+    for i in VLD:
+        VNFD=(i['vnfd-connection-point-ref'])
+        for i in VNFD:
+            print (i['vnfd-id-ref'])
+            #VNFDLIST.get('vnfd-id-ref')
+            VNFDLIST.append(i['vnfd-id-ref'])
+    print(VNFDLIST)
+
+
+
+#VIM_ACCOUNTS=osm_get_vim_accounts(token2)
+
+#print (token3)
 #print(token)
 
-print(geturls('url_projects' ))
+#print(geturls('url_projects' ))
 
-for i in (1,len(VIM_ACCOUNTS)-1):
+#for i in (1,len(VIM_ACCOUNTS)-1):
     #print (VIM_ACCOUNTS[i]['_id'])
     #print (VIM_ACCOUNTS[i]['name'])
     #print (VIM_ACCOUNTS[i]['vim_type'])
     #print (VIM_ACCOUNTS[i]['vim_url'])
 
-    print (time.time())
+    #print (time.time())
