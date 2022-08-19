98fd4861719c0431e27374fba5262aef238e0c6d
Laudelino <laudelinojr@gmail.com>
2022-08-19 18:35:22 -0300

    ajustes

--- RUN_EXPERIMENTwUser.py
+++ RUN_EXPERIMENTwUser.py
@@ -89,16 +89,16 @@ def ExecuteCommand(exec_command):
 #    try:
     print (exec_command)
     ret = subprocess.run(exec_command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, executable='/bin/bash')
-    if (debug_file == 1):
-        print("DEBUG ON")
-        print("CMD - " + exec_command)
-        print("RETURN - \n" + ret.stdout)
-    return ret.returncode
+    #if (debug_file == 1):
+    #    print("DEBUG ON")
+    #    print("CMD - " + exec_command)
+    #    print("RETURN - \n" + ret.stdout)
+    #return ret.returncode
     #except:
     #    print('FAIL IN COMMAND EXECUTE')
     #    print("CMD - " + exec_command)
     #    print("ERROR - " + ret)
-    #return ret.returncode
+    return ret.returncode
 
 def reglog(typemensage):
     if (typemensage != 'cabconfig'):
