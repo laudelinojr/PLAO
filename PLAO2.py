db556e3b69e3d91a30e07d6b6e726773b2aeff3c
Laudelino <laudelinojr@gmail.com>
2022-08-21 15:22:31 -0300

    testes

--- PLAO2.py
+++ PLAO2.py
@@ -1678,7 +1678,7 @@ def main():
             UpdateFinishTestsMethods(METHOD_2_CL2)
 
             now=datetime.now()
-            intervalo=60
+            intervalo=6000
             delta = timedelta(seconds=intervalo)
             #deltagm= timedelta(seconds=10600)
             time_past=now-delta
