db26d21343b521d4f883d09a2c5aad08511dec9e
Laudelino <laudelinojr@gmail.com>
2022-08-21 10:33:33 -0300

    ajustes

--- linux/crontab_plao_client.sh
+++ linux/crontab_plao_client.sh
@@ -2,6 +2,8 @@
 
 chmod 755 /home/laudelinoas/PLAO/linux/crontab_plao_client.sh
 cd /home/laudelinoas/PLAO
+git stash
+git stash clear
 git pull
 
 if [ `ps ax | grep PLAO_client2.py | grep -v grep | wc -l` ==  0 ] ; then
