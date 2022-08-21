e27598609a38a489db6a39d1b5356d031396af94
Laudelino <laudelinojr@gmail.com>
2022-08-21 10:20:39 -0300

    ajustes

--- linux/crontab_plao_client.sh
+++ linux/crontab_plao_client.sh
@@ -3,3 +3,8 @@
 chmod 755 /home/laudelinoas/PLAO/linux/crontab_plao_client.sh
 cd /home/laudelinoas/PLAO
 git pull
+
+if [ `ps ax | grep PLAO_client2.py | grep -v grep | wc -l` ==  0 ] ; then
+ cd /home/laudelinoas/PLAO
+ python3 PLAO_client2.py
+fi
--- linux/crontab_plao_server.sh
+++ linux/crontab_plao_server.sh
@@ -3,3 +3,8 @@
 chmod 755 /opt/PLAO/linux/crontab_plao_server.sh
 cd /opt/PLAO
 git pull
+
+if [ `ps ax | grep PLAO.py | grep -v grep | wc -l` ==  0 ] ; then
+ cd /opt/PLAO/
+ python3 PLAO2.py
+fi
