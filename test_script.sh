3e008c01aa9b9cc0aaef90e1c13f3e53a06d1a3d
Laudelino <laudelinojr@gmail.com>
2022-08-29 16:46:44 -0300

    testes

--- test_script.sh
+++ test_script.sh
@@ -2,7 +2,7 @@
 #CENARIO 1
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST start;
 sleep 5
-python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 10;
+python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 12;
 ##python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST upcpu 200.137.82.21 10;
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST sendjob 200.137.82.11 1;
 ##python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST resetcpu 200.137.82.21;
@@ -16,7 +16,7 @@ sleep 20
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST start;
 sleep 5
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencylink 15;
-python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 10;
+python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 12;
 #python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST upcpu 200.137.82.21 10;
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST sendjob 200.137.82.11 1;
 #python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST resetcpu;
@@ -30,7 +30,7 @@ sleep 20
 #CENARIO 3
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST start;
 sleep 5
-python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 10;
+python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 12;
 #python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST upcpu 200.137.82.21 10;
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST sendjob 200.137.82.11 1;
 #python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST resetcpu  200.137.82.21;
@@ -43,7 +43,7 @@ sleep 20
 #CENARIO 4
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST start;
 sleep 5
-python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 10;
+python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 12;
 #python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST upcpu 200.137.82.21 10;
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST sendjob 200.137.82.11 2;
 #python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST resetcpu 200.137.82.21;
@@ -56,7 +56,7 @@ sleep 20
 #CENARIO 5
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST start;
 sleep 5
-python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 10;
+python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 12;
 #python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST upcpu 200.137.82.21 90;
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST sendjob 200.137.82.11 2;
 #python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST resetcpu 200.137.82.21;
