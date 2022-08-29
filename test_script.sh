9568d0d8ed4acd0e30c62bdbbadc5d738e4fba0d
Laudelino <laudelinojr@gmail.com>
2022-08-29 20:58:16 -0300

    ajuste

--- test_script.sh
+++ test_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#CENARIO 1
+print "INICIO CENARIO 1"
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 10;
 sleep 5
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST start;
@@ -13,7 +13,7 @@ python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST deleteallns;
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST stop;
 sleep 20
 
-#CENARIO 2
+print "INICIO CENARIO 2" 
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencylink 15;
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 10;
 sleep 5
@@ -31,7 +31,7 @@ python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST stop;
 sleep 20
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 10;
 sleep 5
-#CENARIO 3
+print "INICIO CENARIO 3"
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST start;
 sleep 5
 #python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST upcpu 200.137.82.21 10;
@@ -43,7 +43,7 @@ python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST deleteallns;
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST stop;
 sleep 20
 
-#CENARIO 4
+print "INICIO CENARIO 4" 
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 10;
 sleep 5
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST start;
@@ -57,7 +57,7 @@ python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST deleteallns;
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST stop;
 sleep 20
 
-#CENARIO 5
+print "INICIO CENARIO 5"
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST uplatencytouser 200.137.75.159 200.137.82.11 10;
 sleep 5
 python3 PLAO2_test_to_PLAOServer.py 127.0.0.1 POST start;
