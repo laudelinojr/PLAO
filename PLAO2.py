d3fa5bf7d29d6395d91de22fea58b4a75982de93
Laudelino <laudelinojr@gmail.com>
2022-09-01 18:29:25 -0300

    testes

--- PLAO2.py
+++ PLAO2.py
@@ -2086,7 +2086,7 @@ def main():
 
 
                                     with open('log.txt', 'a') as f:
-                                        f.write('\n'.join(i))
+                                        f.write('\n'.join(i['vim-account-id']))
 
 
                                     print(i['vim-account-id'])
