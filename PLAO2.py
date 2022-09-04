a0b649148f664fde3bf5e9086793067061bce782
Laudelino <laudelinojr@gmail.com>
2022-09-04 11:28:59 -0300

    ajustes

--- PLAO2.py
+++ PLAO2.py
@@ -1817,14 +1817,15 @@ def main():
                 #Alterado para testes
                 #Latencia_to_cloud2=cloud1_gnocchi.get_last_measure("Lat_To_"+cloud1.getExternalIp(),cloud1_resource_id,None,GRANULARITY,START,STOP)
                 #print("LatenciatoCloud2: "+str(Latencia_to_cloud2))
+                UpdateFinishTestsMethods(METHOD_3_CL1)
+                
                 Latencia_to_cloud2=cloud2_gnocchi.get_last_measure("Lat_To_"+cloud1.getExternalIp(),cloud2_resource_id,None,GRANULARITY,START,STOP)
                 print("LatenciatoCloud2: "+str(Latencia_to_cloud2))
                 Jitter_to_cloud2=cloud1_gnocchi.get_last_measure("Jit_To_"+cloud2.getExternalIp(),cloud1_resource_id,None,GRANULARITY,START,STOP)
                 print("JittertoCloud2: "+str(Jitter_to_cloud2))
-                
                 PILFile.SearchChangePriceLatencyJitterPIL(Latencia_to_cloud2,Latencia_to_cloud2,Jitter_to_cloud2,"openstackSerra","openstackAracruz2",TEST_ID)
                 #time.sleep(20)
-                UpdateFinishTestsMethods(METHOD_3_CL1)
+                #UpdateFinishTestsMethods(METHOD_3_CL1)
 
             if(cloud2.getStatus()==1):
                 METHOD_3_CL2=InsertTestsMethods(TEST_ID,3,2)
@@ -2081,40 +2082,40 @@ def main():
                             #time.sleep(1)
                             if i['nsr-id-ref'] == id_ns_scheduled['id']:
                                 if i['_admin']['nsState'] == 'INSTANTIATED':
-                                    print(i['vim-account-id'])
+                                    ####print(i['vim-account-id'])
                                     InsertVnfInstanciated(i['_id'],i['vnfd-ref'],GetIdCloudbyvimidosm(i['vim-account-id']),1,id_ns_instanciated)
-                                    print("imprimindo teste")
-                                    #print(test)
-                                    print("imprimindo VNFs")
+                                    ####print("imprimindo teste")
+                                    ###print(test)
+                                    ###print("imprimindo VNFs")
                                     #print(i)
-                                    print("id vnf")
-                                    print (i['_id'])
-                                    print(i['ip-address'])
-                                    print("vim account id")
-                                    print(i['vim-account-id'])
-                                    print(VNF_ALREADY)
+                                    #print("id vnf")
+                                    #print (i['_id'])
+                                    #print(i['ip-address'])
+                                    #print("vim account id")
+                                    #print(i['vim-account-id'])
+                                    #print(VNF_ALREADY)
                                     if i['ip-address'] is not None:
-                                        print(i['ip-address'])
+                                        ###print(i['ip-address'])
                                         if not (VNF_ALREADY.__contains__(i['ip-address'])):
                                             if (i['vim-account-id']=="9f104eee-5470-4e23-a8dd-3f64a53aa547"):
                                                 VNF_ALREADY.append(i['ip-address'])
-                                                print("registrando cloud 1")
-                                                print("eh este 9f104eee-5470-4e23-a8dd-3f64a53aa547")
+                                                #print("registrando cloud 1")
+                                                #print("eh este 9f104eee-5470-4e23-a8dd-3f64a53aa547")
                                                 InsertActionsTests(TEST_ID,6,datetime.timestamp(datetime.now().utcnow())) #Registrar Instanciado nuvem 1
                                                 UpdateFinishTestsMethodsifNone(METHOD_12_CL1) #CL1                                    
                                             if (i['vim-account-id']=="6ba02d24-6320-4322-9177-eb4987ad9465"):
                                                 VNF_ALREADY.append(i['ip-address'])
-                                                print("registrando cloud 2")
-                                                print("eh este 6ba02d24-6320-4322-9177-eb4987ad9465")
+                                                #print("registrando cloud 2")
+                                                #print("eh este 6ba02d24-6320-4322-9177-eb4987ad9465")
                                                 InsertActionsTests(TEST_ID,7,datetime.timestamp(datetime.now().utcnow())) #Registrar instanciado nuvem 2
                                                 UpdateFinishTestsMethodsifNone(METHOD_12_CL2) #CL2
                                                 
-                                    print(VNF_ALREADY)
-                                    print(i['_admin']['nsState'])
+                                    #print(VNF_ALREADY)
+                                    #print(i['_admin']['nsState'])
                         if(len(VNF_ALREADY)==2):
                             out2=True
                     if(test['nsState'] == "READY"):
-                        print("VNFS ALREADY and nState READY")
+                        ###print("VNFS ALREADY and nState READY")
                         InsertActionsTests(TEST_ID,5,datetime.timestamp(datetime.now().utcnow())) #Registrar acao ao ser executado/Criar outro tipo para finalizado
                         UpdateNsInstanciated(id_ns_scheduled,2,JOB_COD)
                         out=True
