f2132376de25d53a301af2ac3976a14e4a25d254
Laudelino <laudelinojr@gmail.com>
2022-10-04 10:18:16 -0300

    d

--- PLAO2.py
+++ PLAO2.py
@@ -800,7 +800,7 @@ def InsertVnfInstanciated(id_osm0,name_osm,id_fk_cloud,id_status,id_ns_instancia
 
 def SelectVnfInstanciatedExists(cod,vnf):
     selectifExists=""
-    selectifExists=(Vnf_Instanciateds.select().where(Vnf_Instanciateds.fk_ns_instanciated==cod).dicts().get())
+    selectifExists=(Vnf_Instanciateds.select(Vnf_Instanciateds.fk_ns_instanciated).where(Vnf_Instanciateds.fk_ns_instanciated==cod).dicts().get())
     
     #.where((Vnf_Instanciateds.fk_ns_instanciated==cod)&(Vnf_Instanciateds.name_osm_vnf_instanciated==vnf)).dicts().get()
     # if 'id_vnf_instanciated' in selectifExists:
